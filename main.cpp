ab0f744595005042157b5db2fbbb2ee15694de04
🌿 Edmundo Canedo Cervantes <edcance.dev@gmail.com>
2025-03-08 21:32:00 -0600

    Inicio evidencia

---
+++ main.cpp
@@ -0,0 +1,33 @@
+/*Edmundo Canedo Cervantes - A01645576 */
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+class storedInput {
+    private:
+    
+
+    public:
+};
+
+vector<char> getAlphabet(){
+    vector<char> alphabet;
+    map<char, bool> alreadyOn;
+    string aux;
+
+    cin >> aux;
+
+    for(int i = 0; i < aux.size(); i++) {
+
+    }
+}
+
+int main(){
+
+    cout << "Type the alphabet for the RegEx: ";
+    vector<char> alphabet = getAlphabet();
+
+    cout<<"\n";
+    return 0;
+}
